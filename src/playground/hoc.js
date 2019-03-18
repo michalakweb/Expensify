d5bb6f31a52376b1eded129c9fc74fa62e514d12
Mateusz Michalak <michalakweb@gmail.com>
2019-03-18 12:17:57 +0100

    hoc2

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const info = () => (
+const Info = (props) => (
     <div>
-    
+        <h1>Info</h1>
+        <p>The info is: {props.info}</p>
     </div>
-)
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>don't share</p>}
+            <WrappedComponent {...props}/>
+        </div>
+    );
+};
+
+const requireAuth = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? 
+                <WrappedComponent {...props} /> : 
+                <p>Sorry, no access</p>
+            }
+        </div>
+    )
+}
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuth(Info);
+
+ReactDOM.render(<AuthInfo isAuthenticated={false} info="details" />, document.querySelector('#app'));
